8cace52e98e083123d725ad5ec0cbc2f0b5b1ce7
John Kevin Javier <john_kevin_javier@yahoo.com>
2022-12-05 16:07:24 +0800

    Remove reference of other dev

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,9 +8,9 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi, I'm <span className="purple">Michael Beebower </span>
-            from <span className="purple"> Austin, Texas.</span>
-            <br />I'm a 4th-year student, pursuing a <a href="https://www6.austincc.edu/cms/site/www/catalog/programmaps/programmap.php?ap=7049&yr=2022">Bachelors degree in Software Development at ACC</a>.
+            Hi, I'm <span className="purple">John Kevin Javier </span>
+            from <span className="purple"> Philippines.</span>
+            <br />I'm a Full Stack Software Engineer.
             <br />
             <br />
             <br />
@@ -18,20 +18,20 @@ function AboutCard() {
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Camping
+              <ImPointRight /> Free Diving
             </li>
             <li className="about-activity">
               <ImPointRight /> Traveling
             </li>
             <li className="about-activity">
-              <ImPointRight /> Listening to Music
+              <ImPointRight /> Playing Console Games
             </li>
           </ul>
 
           <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
             "All repetitive, menial computer tasks can most likely be automated or partially automated with the right code."{" "}
           </p>
-          <footer className="blockquote-footer">Michael Beebower</footer>
+          <footer className="blockquote-footer">John Kevin Javier</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,16 +14,16 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copyright">
-          <h3>Developed by Michael Beebower</h3>
+          <h3>Developed by John Kevin Javier</h3>
         </Col>
         <Col md="4" className="footer-copyright">
-          <h3>Copyright © {year} MB</h3>
+          <h3>Copyright © {year} John Kevin Javier</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/beebus/"
+                href="https://github.com/johnjavier/"
                 style={{ color: "white" }}
               >
                 <AiFillGithub />
@@ -31,7 +31,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/michael-beebower/"
+                href="https://www.linkedin.com/in/john-kevin-javier-83b70263/"
                 style={{ color: "white" }}
               >
                 <FaLinkedinIn />
