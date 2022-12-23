87f33025e6f3b934bd2f152e303bd51a8c10b671
John Kevin Javier <john_kevin_javier@yahoo.com>
2022-12-23 14:47:15 +0800

    Remove unnecessary assets

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Resume_Michael_Beebower.pdf";
+import pdf from "../../Assets/John_Kevin_Javier_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/beebus/portfolio/master/src/Assets/Resume_Michael_Beebower.pdf";
+  "https://raw.githubusercontent.com/johnjavier/Portfolio/master/src/Assets/John_Kevin_Javier_Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
